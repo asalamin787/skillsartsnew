61b0398ecb270f74877ae714a50b491524917234
Alamin Sikder <117014662+asalamin787@users.noreply.github.com>
2024-12-29 23:45:14 +0600

    remove

--- resources/views/layouts/homeapp.blade.php
+++ resources/views/layouts/homeapp.blade.php
@@ -38,7 +38,7 @@
 
     {{-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css"> --}}
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    {{-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.9.0/css/all.min.css"> --}}
+
     {{-- @livewireStyles --}}
 
     <style>
